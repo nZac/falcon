66547efeebc71912db969919ab92286d404abba4
Nick Zaccardi <nicholas.zaccardi@gmail.com>
2018-12-08 17:57:31 -0600

    Enable custom serialization and deserialization functions

--- falcon/media/json.py
+++ falcon/media/json.py
@@ -10,13 +10,35 @@ from falcon.util import json
 class JSONHandler(BaseHandler):
     """JSON media handler.
 
-    This handler uses Python's :py:mod:`json` by default, but will
-    use :py:mod:`ujson` if available.
+    Keyword Arguments:
+        dumps(function): a function which serializes response media into JSON format. By default
+            this uses :py:mod:`json` with `ensure_ascii` disabled.
+
+        loads(function): a function which consumes and deserializes the request stream from JSON
+            format. By Default this uses :py:mod:`json`
+
+        dumps_args(dict): other arguments to pass to `dumps` during serialization.
+
+        loads_args(dict): other arguments to pass to `loads` during deserialization.
     """
+
+    def __init__(self, dumps=None, loads=None, dumps_args=None, loads_args=None):
+        self.dumps_args = {
+            'enable_ascii': False
+        }
+        self.dumps_args.update(dump_args)
+
+        self.loads_args = {
+            'enable_ascii': False
+        }
+        self.loads_args.update(loads_args)
+
+        self.dumps = dumps or json.dumps
+        self.loads = loads or json.loads
 
     def deserialize(self, stream, content_type, content_length):
         try:
-            return json.loads(stream.read().decode('utf-8'))
+            return self.loads(stream.read().decode('utf-8'), **self.loads_args)
         except ValueError as err:
             raise errors.HTTPBadRequest(
                 'Invalid JSON',
@@ -24,7 +46,7 @@ class JSONHandler(BaseHandler):
             )
 
     def serialize(self, media, content_type):
-        result = json.dumps(media, ensure_ascii=False)
+        result = self.dumps(media, **self.dumps_args)
         if six.PY3 or not isinstance(result, bytes):
             return result.encode('utf-8')
 
